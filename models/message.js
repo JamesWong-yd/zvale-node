822928fbdfd773405f2a0c416cf2a188b7bf1ada
James Wong <380928526@qq.com>
2018-04-08 21:20:40 +0800

    fix message

--- models/message.js
+++ models/message.js
@@ -5,50 +5,21 @@ const messageSchema = new Schema({
   title: String,
   content: String,
   type: String,
-  state: {
-    type: Number,
-    default: 1,
-  },
-  sendDate: {
-    type: Date,
-    default: new Date()
-  },
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: 'account'
-  },
-  createTime: {
-    type: Date,
-    default: new Date()
-  },  // yyyyMMdd hh:mm:ss
+  state: { type: Number, default: 1},
+  sendDate: {type: Date, default: new Date()},
+  author: {type: Schema.Types.ObjectId, ref: 'account'},
+  createTime: {type: Date,default: new Date()},  // yyyyMMdd hh:mm:ss
   removeTime: Date,
-  receiver: [{
-    type: Schema.Types.ObjectId,
-    ref: 'account'
-  }]
+  receiver: [{type: Schema.Types.ObjectId,ref: 'account'}]
 });
 
 const msgStateSchema = new Schema({
-  read: {
-    type: Number,
-    default: 1
-  }, //1 未读   2 已读   3 垃圾箱
-  state: {
-    type: Number,
-    default: 1,
-  }, // 1有效 0无效
-  messageId: {
-    type: Schema.Types.ObjectId,
-    ref: 'message'
-  },
-  accountId: {
-    type: Schema.Types.ObjectId,
-    ref: 'account'
-  },
-  readTime: {
-    type: Date,
-    default: ''
-  } // yyyyMMdd hh:mm:ss
+  read: {type: Number, default: 1 }, //1 未读   2 已读   3 垃圾箱
+  state: {type: Number, default: 1}, // 1有效 0无效
+  messageId: {type: Schema.Types.ObjectId,ref: 'message'},
+  accountId: {type: Schema.Types.ObjectId,ref: 'account'},
+  readTime: {type: Date,default: ''}, // yyyyMMdd hh:mm:ss
+  createTime: {type: Date,default: new Date() } // yyyyMMdd hh:mm:ss
 })
 
 const message = mongoose.model('message', messageSchema)
